de49be6aca68c007f4c2afa18816bde8c4f4bdd4
senkxv <senkxv@gmail.com>
2026-04-04 20:19:18 +0300

    Create rediraction to UserWorkspaceIndex

---
+++ Controllers/UserWorkspaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blank.Controllers
+{
+    public class UserWorkspaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
