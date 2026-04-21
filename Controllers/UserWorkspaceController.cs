a673a09de465e257aaaca874a1c6c896b9bedf31
senkxv <senkxv@gmail.com>
2026-04-21 08:34:50 +0300

    Finich "EditDocuentPage"

--- Controllers/UserWorkspaceController.cs
+++ Controllers/UserWorkspaceController.cs
@@ -58,5 +58,52 @@ namespace Blank.Controllers
 
             return View(document);
         }
+
+        // GET: /UserWorkspace/Edit/5
+        // GET: /UserWorkspace/EditDocumentPage/5
+        public IActionResult EditDocumentPage(int id)
+        {
+            var документ = _context.Документы.Find(id);
+            if (документ == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.DocumentTypes = _context.Типы_Документов.ToList();
+            ViewBag.Organizations = _context.Организации.ToList();
+            ViewBag.Drivers = _context.Водители.ToList();
+            ViewBag.Transport = _context.Транспорт.ToList();
+            ViewBag.LoadingPoints = _context.Пункт_Погрузки.ToList();
+            ViewBag.UnloadingPoints = _context.Пункт_Разгрузки.ToList();
+
+            return View(документ);
+        }
+
+        // POST: /UserWorkspace/EditDocumentPage/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditDocumentPage(int id, Documents document)
+        {
+            if (id != document.ид_документа)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(document);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DocumentTypes = _context.Типы_Документов.ToList();
+            ViewBag.Organizations = _context.Организации.ToList();
+            ViewBag.Drivers = _context.Водители.ToList();
+            ViewBag.Transport = _context.Транспорт.ToList();
+            ViewBag.LoadingPoints = _context.Пункт_Погрузки.ToList();
+            ViewBag.UnloadingPoints = _context.Пункт_Разгрузки.ToList();
+
+            return View(document);
+        }
     }
 }
