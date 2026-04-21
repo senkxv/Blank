1126354f04ef775da1161f62114b929ea2e7c9e4
senkxv <senkxv@gmail.com>
2026-04-21 06:30:31 +0300

    fix bug (change action in form on CreateDocumentPage)

--- Models/Tables/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Blank.Models.Tables
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
