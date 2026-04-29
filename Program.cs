7cee2839218ff5a32c46284b50b54c72b9986397
senkxv <senkxv@gmail.com>
2026-04-29 10:05:44 +0300

    Delete all comments from Program.cs

--- Program.cs
+++ Program.cs
@@ -4,12 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавляем DbContext для MySQL
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseMySQL(builder.Configuration.GetConnectionString("LocalConnection"))
 );
 
-// Добавляем сервисы для работы с сессиями
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
@@ -22,7 +20,6 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Включаем подробные ошибки для разработки
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
@@ -36,7 +33,6 @@ else
 app.UseHttpsRedirection();
 app.UseRouting();
 
-// Добавляем UseSession перед UseAuthorization
 app.UseSession();
 
 app.UseAuthorization();
