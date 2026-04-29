c0444e765069368745e9380403ff8a127c1d8ab1
senkxv <senkxv@gmail.com>
2026-04-29 11:02:34 +0300

    Delete required attributes from LoginViewModel

--- Models/ViewModels/LoginViewModel.cs
+++ Models/ViewModels/LoginViewModel.cs
@@ -4,10 +4,8 @@ namespace Blank.Models.ViewModels
 {
     public class LoginViewModel
     {
-        [Required]
         public string? Email { get; set; }
 
-        [Required]
         [DataType(DataType.Password)]
         public string? Password { get; set; }
 
