0511e43634106dccf4ed126c2f795f4b46eaaa48
senkxv <senkxv@gmail.com>
2026-04-29 19:18:38 +0300

    Update view of registration and authorization

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
-    options.UseMySQL(builder.Configuration.GetConnectionString("LocalConnection") ?? "")
+    options.UseMySQL(builder.Configuration.GetConnectionString("RemoteConnection") ?? "")
 );
 
 builder.Services.AddDistributedMemoryCache();
