56a0752abeb4df6e5ab530cbaa8a70ca686cf38c
senkxv <senkxv@gmail.com>
2026-04-29 12:22:21 +0300

    Update RegisterViewModel.cs: create "string?"

--- Models/ViewModels/RegisterViewModel.cs
+++ Models/ViewModels/RegisterViewModel.cs
@@ -6,19 +6,19 @@ namespace Blank.Models.ViewModels
     {
         [Required(ErrorMessage = "Введите email")]
         [EmailAddress(ErrorMessage = "Введите корректный email")]
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         [Required(ErrorMessage = "Введите пароль")]
         [DataType(DataType.Password)]
         [MinLength(6, ErrorMessage = "Пароль должен быть не менее 6 символов")]
-        public string Password { get; set; }
+        public string? Password { get; set; }
 
         [Required(ErrorMessage = "Подтвердите пароль")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Пароли не совпадают")]
-        public string ConfirmPassword { get; set; }
+        public string? ConfirmPassword { get; set; }
 
         [Required(ErrorMessage = "Введите ФИО")]
-        public string ФИО { get; set; }
+        public string? ФИО { get; set; }
     }
 }
