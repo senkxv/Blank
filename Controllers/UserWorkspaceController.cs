71ee47aa03313e67cf4ea82d57afbdec33a6414d
senkxv <senkxv@gmail.com>
2026-05-03 20:59:07 +0300

    q

--- Controllers/UserWorkspaceController.cs
+++ Controllers/UserWorkspaceController.cs
@@ -28,10 +28,10 @@ namespace Blank.Controllers
             _context = context;
         }
 
-        public IActionResult Error404()
+        /*public IActionResult Error404()
         {
             return View();
-        }
+        }*/
 
         /*public IActionResult Error500()
         {
